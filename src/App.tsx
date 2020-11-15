8c8de450ae056333665200734e68681f3de71bee
Beth <44749113+laceswingybethler@users.noreply.github.com>
2020-11-15 22:28:23 +0000

    Initial Scatterplot

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { PlotContainer } from './components/Scatterplot';
 
+const mockData = [
+  { day: '2017-04-18', productPerceivedQuality: '2.8' },
+  { day: '2017-04-19', productPerceivedQuality: '2.9' },
+  { day: '2017-04-20', productPerceivedQuality: '2.7' },
+  { day: '2017-04-21', productPerceivedQuality: '4.3' },
+  { day: '2017-04-22', productPerceivedQuality: '4.6' },
+  { day: '2017-04-23', productPerceivedQuality: '5' },
+  { day: '2017-04-24', productPerceivedQuality: '5.2' },
+  { day: '2017-04-25', productPerceivedQuality: '5.1' },
+  { day: '2017-04-26', productPerceivedQuality: '4.8' },
+  { day: '2017-04-27', productPerceivedQuality: '4.9' },
+  { day: '2017-04-28', productPerceivedQuality: '5.1' },
+  { day: '2017-04-29', productPerceivedQuality: '5.3' },
+  { day: '2017-04-30', productPerceivedQuality: '5.6' },
+  { day: '2017-05-01', productPerceivedQuality: '6.2' },
+];
+
+// This guy is not good enough at TypeScript
+// ttps://hackernoon.com/how-and-why-to-use-d3-with-react-d239eb1ea274
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlotContainer
+        data={mockData}
+        height={40}
+        selectX={datum => new Date(datum.day)}
+        selectY={datum => parseFloat(datum.productPerceivedQuality)}
+        width={200}
+      />
     </div>
   );
 }
---
+++ src/components/Scatterplot.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { extent as d3ArrayExtent } from 'd3-array';
+import {
+  scaleLinear as d3ScaleLinear,
+  scaleTime as d3ScaleTime,
+} from 'd3-scale';
+import { line as d3Line } from 'd3-shape';
+import { axisBottom, axisLeft } from 'd3-axis'; // d3-axis does mutate the DOM.
+import { select } from 'd3-selection';
+
+type NumberValue = number | { valueOf(): number };
+
+type Datum = {
+  day: Date;
+  productPerceivedQuality: NumberValue;
+};
+
+// type Data = Datum[]; // durt
+type Data = [NumberValue, NumberValue][]; // I think it's gon have to be a Map/ arr of nums. Cause it need length
+
+type Props = {
+  data: Data; // this actually needs to be like the VALUES
+  height: number;
+  selectX: (datum: Pick<Datum, 'day'>) => Date;
+  selectY: (datum: Pick<Datum, 'productPerceivedQuality'>) => NumberValue; // not happy about this any type
+  width: number;
+};
+
+export const PlotContainer = ({
+  data,
+  height,
+  selectX,
+  selectY,
+  width,
+}: Props) => {
+  // sets the domain. Extent returns the ([min, max]) of x (time) values (AKA dates) in the data.
+  // range will be from x=0 to x=width (so this is why width is not pixels.)
+  const xScale = d3ScaleTime()
+    .domain(d3ArrayExtent(data, selectX))
+    .range([0, width]);
+
+  const yScale = d3ScaleLinear() // a classic linear scale
+    .domain(d3ArrayExtent(data, selectY))
+    .range([height, 0]);
+
+  const selectScaledX = datum => xScale(selectX(datum));
+  const selectScaledY = datum => yScale(selectY(datum));
+
+  // x axis should have half as many ticks as rows in the data
+  const xAxis = axisBottom(xScale).ticks(data.length / 2);
+
+  const yAxis = axisLeft(yScale).ticks(3);
+
+  // a line factor...
+  const sparkLine = d3Line().x(selectScaledX).y(selectScaledY);
+
+  const linePath = sparkLine(data); // Data needs to be [number, number][] | Iterable<[number, number]>
+
+  return (
+    <svg className="container" height={height} width={width}>
+      {/* g is a group node, for convenient styling */}
+
+      {/* Axes won't re-render if data updates because we're rendering them directly into the groups. */}
+      <g className="xAxis" ref={node => select(node).call(xAxis)} />
+      <g className="yAxis" ref={node => select(node).call(yAxis)} />
+
+      <g className="line">
+        <path d={linePath} />
+      </g>
+    </svg>
+  );
+};
